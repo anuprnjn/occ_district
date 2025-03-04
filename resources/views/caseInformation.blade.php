9692aefac6c7202d457dc41fcf004c585e819bfa
Tushar Anand <tusharanand@Tushars-MacBook-Air.local>
2025-03-04 12:21:46 +0530

    updated route

--- resources/views/caseInformation.blade.php
+++ resources/views/caseInformation.blade.php
@@ -251,7 +251,7 @@ function handleCaseInformationSubmit(event) {
         btnSpinner.classList.add("hidden");
         submitBtn.disabled = false;
 
-        window.location.href = '/occ/CDPay';
+        window.location.href = '/occ/cd_pay';
     }, 1500);
 }
 
