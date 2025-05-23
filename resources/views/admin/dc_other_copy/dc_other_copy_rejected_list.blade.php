a61e1bf251b70b922b474d32dff4d3f62ee3f9d3
kgautam8 <gautamkumar41996@gmail.com>
2025-05-23 12:53:53 +0530

    heading in rejected dc

--- resources/views/admin/dc_other_copy/dc_other_copy_rejected_list.blade.php
+++ resources/views/admin/dc_other_copy/dc_other_copy_rejected_list.blade.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Crypt;
     <div class="app-content-header">
       <div class="container-fluid">
         <div class="row">
-          <div class="col-sm-6"><h3 class="mb-0">Civilcourt Other Copy Rejected Application</h3></div>
+          <div class="col-sm-6"><h3 class="mb-0">Other Copy Rejected Application</h3></div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-end">
               <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active" aria-current="page">HC Other Copy</li>
+              <li class="breadcrumb-item active" aria-current="page">Other Copy</li>
             </ol>
           </div>
         </div>
